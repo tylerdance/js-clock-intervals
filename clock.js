4a280b4e1de189f6feaa92200767eaea94a86fd6
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-10-23 12:25:36 -0400

    functions WIP

---
+++ clock.js
@@ -0,0 +1,51 @@
+
+
+
+
+function secondRotation(seconds) {
+    var second = (date.getSeconds() + date.getMilliseconds()/1000) /60 * 360
+    setInterval(function) {
+        angle += 6
+    }
+
+      return degRotation;
+    
+    // 6 degrees per second
+    }
+
+
+
+
+
+
+function minuteRotation(minutes, seconds) {
+    var minutes = (date.getMinutes() / 60 * 360)
+    setInterval function() {
+        for (i = 0; i < minuteRotation.length; i++)
+        angle += 6
+
+        return degRotation
+    // first, find the total seconds by converting the minutes to seconds then adding that value to the seconds argument
+    //  code here
+    
+    // there are 60sec*60min=3600 seconds in a hour
+      // that means, each second needs to rotate the
+      // minutehand by 360/3600=.1 degrees
+    
+      // code here to find the degrees of rotation
+    // don’t forget to return the final value!
+    
+    }
+
+
+function hourRotation(hours, minutes) {
+      // first, find the total minutes converting the hours to minutes, then adding that value to the minutes argument
+      // code here
+    
+      // there are 60min*12hr=720 minutes in a full rotation (12 hours)
+      // that means, each minute needs to rotate the
+      // minutehand by 360/720=.5 degrees
+    
+      // code here to find the degrees of rotation
+        // don’t forget to return the final value!
+    }
